0962de1f14e24a718882276508d3bf221917f289
Steve Agalloco <steve.agalloco@gmail.com>
2011-02-19 15:47:38 -0500

    simple refactor

--- lib/tophat/meta.rb
+++ lib/tophat/meta.rb
@@ -3,13 +3,12 @@ module TopHat
 
     # page descriptions
     # <meta name="description" content="Description goes here." />
-    def description(options=nil)
+    def description(options={})
       if options.is_a? String
         @tophat_description = options
 
       else
-        options ||= {}
-        options.merge!(:name => 'description')
+        options[:name] = 'description'
 
         if @tophat_description.blank?
           options.merge!(:content => options.delete(:default))
@@ -24,28 +23,26 @@ module TopHat
 
     # keywords
     # <meta name="keywords" content="Keywords go here." />
-    def keywords(options=nil)
+    def keywords(options={})
       if options.is_a?(String)
         @tophat_keywords = options
 
       elsif options.is_a?(Array)
-        @tophat_keywods = options.join(', ')
+        @tophat_keywords = options.join(', ')
 
       else
-        options ||= {}
-        options.merge!(:name => 'keywords')
-
+        options[:name] = 'keywords'
         default_keywords = options.delete(:default) || []
         display_keywords = @tophat_keywords.blank? ? default_keywords : @tophat_keywords
 
         # normalize the keywords
-        default_keywords = default_keywords.is_a?(String) ? default_keywords.split(', ') : default_keywords
-        display_keywords = display_keywords.is_a?(String) ? display_keywords.split(', ') : display_keywords
+        default_keywords = default_keywords.is_a?(String) ? default_keywords.split(',') : default_keywords
+        display_keywords = display_keywords.is_a?(String) ? display_keywords.split(',') : display_keywords
 
         # merge keyword arrays if merge is set to true
         display_keywords += default_keywords if options.delete(:merge_default) == true
 
-        options.merge!(:content => display_keywords.uniq.join(', '))
+        options.merge!(:content => display_keywords.uniq.join(', ').squeeze(' '))
         tag(:meta, options) if display_keywords.any?
       end
     end
