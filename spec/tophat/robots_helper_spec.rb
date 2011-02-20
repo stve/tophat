ce7f72fa8d89e19d5e86ffb8d0963035165d9efe
Steve Agalloco <steve.agalloco@gmail.com>
2011-02-19 21:16:46 -0500

    robots helper spec

---
+++ spec/tophat/robots_helper_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe TopHat::RobotsHelper do
+
+  before(:all) do
+    public_dir = File.join(File.dirname(__FILE__), 'public')
+    @helpers = ActionController::Base.helpers
+    @helpers.config.perform_caching = false
+    @helpers.config.assets_dir = public_dir
+    @helpers.config.javascripts_dir = "#{public_dir}/javascripts"
+    @helpers.config.stylesheets_dir = "#{public_dir}/stylesheets"
+
+    @template = ActionView::Base.new
+  end
+
+  describe ".nofollow" do
+    it "should default to all robots" do
+      @template.nofollow.should == "<meta content=\"nofollow\" name=\"robots\" />"
+    end
+
+    it "should use a descriptor if one is provided" do
+      @template.nofollow('googlebot').should == "<meta content=\"nofollow\" name=\"googlebot\" />"
+    end
+  end
+
+  describe ".noindex" do
+    it "should default to all robots" do
+      @template.noindex.should == "<meta content=\"noindex\" name=\"robots\" />"
+    end
+
+    it "should use a descriptor if one is provided" do
+      @template.noindex('googlebot').should == "<meta content=\"noindex\" name=\"googlebot\" />"
+    end
+  end
+
+  describe ".canonical" do
+    it "should not render when not passed a path" do
+      @template.canonical.should be_nil
+    end
+
+    it "should render a tag when passed a path" do
+      @template.canonical('http://mysite.com/somepath/').should == "<link href=\"http://mysite.com/somepath/\" rel=\"canonical\" />"
+    end
+  end
+
+end
