d7c67d621c531d681e5bb4de0d178cbbfedf1dd6
Steve Agalloco <steve.agalloco@gmail.com>
2012-08-29 23:02:10 -0400

    split keywords method out into a class

--- lib/tophat/meta.rb
+++ lib/tophat/meta.rb
@@ -30,6 +30,45 @@ module TopHat
       end
     end
 
+    class Keywords
+      def initialize(keywords, options={})
+        @options = options
+        @keywords = keywords || default_keywords
+      end
+
+      def to_s
+        wordlist.uniq.join(', ').squeeze(' ')
+      end
+
+      def any?
+        wordlist.size > 0
+      end
+
+      private
+
+      def regular_keywords
+        @regulars ||= @keywords.blank? ? default_keywords : @keywords
+      end
+
+      def default_keywords
+        @defaults ||= @options.delete(:default) || []
+      end
+
+      def wordlist
+        @words ||= [].tap do |list|
+          list.concat(normalize(regular_keywords))
+          list.concat(normalize(default_keywords)) if @options[:merge_default] == true
+        end
+      end
+
+      def normalize(list)
+        case list
+        when String then list.split(',')
+        else list
+        end
+      end
+    end
+
     # keywords
     # <meta name="keywords" content="Keywords go here." />
     def keywords(options={})
@@ -41,19 +80,22 @@ module TopHat
         TopHat.current['keywords'] = options.join(', ')
 
       else
-        options[:name] = 'keywords'
-        default_keywords = options.delete(:default) || []
-        display_keywords = TopHat.current['keywords'].blank? ? default_keywords : TopHat.current['keywords']
+        # options[:name] = 'keywords'
+        # default_keywords = options.delete(:default) || []
+        # display_keywords = TopHat.current['keywords'].blank? ? default_keywords : TopHat.current['keywords']
 
-        # normalize the keywords
-        default_keywords = default_keywords.is_a?(String) ? default_keywords.split(',') : default_keywords
-        display_keywords = display_keywords.is_a?(String) ? display_keywords.split(',') : display_keywords
+        # # normalize the keywords
+        # default_keywords = default_keywords.is_a?(String) ? default_keywords.split(',') : default_keywords
+        # display_keywords = display_keywords.is_a?(String) ? display_keywords.split(',') : display_keywords
 
-        # merge keyword arrays if merge is set to true
-        display_keywords += default_keywords if options.delete(:merge_default) == true
+        # # merge keyword arrays if merge is set to true
+        # display_keywords += default_keywords if options.delete(:merge_default) == true
 
-        options.merge!(:content => display_keywords.uniq.join(', ').squeeze(' '))
-        meta_tag(options) if display_keywords.any?
+        # options.merge!(:content => display_keywords.uniq.join(', ').squeeze(' '))
+        # meta_tag(options) if display_keywords.any?
+
+        words = Keywords.new(TopHat.current['keywords'], options)
+        meta_tag(:name => 'keywords', :content => words.to_s) if words.any?
       end
     end
   end
