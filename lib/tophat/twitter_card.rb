d98a7bc83c78f519fb47ee49891a6f23561df467
Steve Agalloco <steve.agalloco@gmail.com>
2013-03-25 23:45:51 -0400

    default twitter cards, fixes #5

--- lib/tophat/twitter_card.rb
+++ lib/tophat/twitter_card.rb
@@ -16,9 +16,11 @@ module TopHat
       def to_html
         output = ActiveSupport::SafeBuffer.new
         output << tag(:meta, :name => 'twitter:card', :value => @type)
+        @card_data = @card_data.delete_if { |k, v| v.nil? }
         @card_data.each do |key, value|
+          tag_name = "twitter:#{key}".squeeze(":")
           output << "\n".html_safe
-          output << tag(:meta, :name => "twitter:#{key}", :value => value)
+          output << tag(:meta, :name => tag_name, :value => value)
         end
         output << "\n".html_safe unless @card_data.empty?
         output
@@ -40,13 +42,12 @@ module TopHat
     end
 
     def twitter_card(type=nil, &block)
-      if type.nil?
-        if TopHat.current['twitter_card']
-          TopHat.current['twitter_card'].to_html
-        end
-      else
+      if TopHat.current['twitter_card'].nil?
         TopHat.current['twitter_card'] = TwitterCardGenerator.new(type, &block)
+      else
+        TopHat.current['twitter_card'].add_nested_attributes('', &block)
       end
+      TopHat.current['twitter_card'].to_html
     end
   end
 end
