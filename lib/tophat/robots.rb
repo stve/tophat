29f0d9a53cad324de26c4811984ee3b4c6eea36f
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-09-18 22:44:40 +0600

    renaming set_tag to robot_tag and avoiding 'seattle style' method definitions

--- lib/tophat/robots.rb
+++ lib/tophat/robots.rb
@@ -4,14 +4,14 @@ module TopHat
     DEFAULT_DESCRIPTOR = 'robots'
 
     def noindex(descriptor = nil, content = 'noindex')
-      set_tag(descriptor, content)
+      robot_tag descriptor, content
     end
 
     def nofollow(descriptor = nil, content = 'nofollow')
-      set_tag(descriptor, content)
+      robot_tag descriptor, content
     end
 
-    def set_tag descriptor = nil, content
+    def robot_tag descriptor = nil, content
       if descriptor
         TopHat.current[content] = descriptor || DEFAULT_DESCRIPTOR
       else
